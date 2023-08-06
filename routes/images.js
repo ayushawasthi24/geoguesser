50751ca71bb8e5b251024c4ebecdcd50d559bccb
Pradeep_Kumar_Rebbavarapu <rpkiit2022@gmail.com>
2023-08-06 13:38:40 +0530

    added about us

--- routes/images.js
+++ routes/images.js
@@ -1,167 +1,167 @@
 const images = [
+  {
+      "_id": 1,
+      "path": "https://res.cloudinary.com/dqo9rn5lp/image/upload/v1691299715/geoguesserimages-pclub/1.jpg",
+      "long": 22.5319188,
+      "lat": 75.9234285
+    },
     {
-        "_id": 1,
-        "path": "https://res.cloudinary.com/dqo9rn5lp/image/upload/v1691299715/geoguesserimages-pclub/1.jpg",
-        "long": 22.5319188,
-        "lat": 75.9234285
-      },
-      {
-        "_id": 2,
-        "path": "https://res.cloudinary.com/dqo9rn5lp/image/upload/v1691299710/geoguesserimages-pclub/2.jpg",
-        "long": 22.5235085,
-        "lat": 75.9254713
-      },
-      {
-        "_id": 3,
-        "path": "https://res.cloudinary.com/dqo9rn5lp/image/upload/v1691299713/geoguesserimages-pclub/3.jpg",
-        "long":22.5303081,
-        "lat":75.9230680
-      },
-      {
-        "_id": 4,
-        "path": "https://res.cloudinary.com/dqo9rn5lp/image/upload/v1691299711/geoguesserimages-pclub/4.jpg",
-        "long": 22.5202184,
-        "lat": 75.9204375
-      },
-      {
-        "_id": 5,
-        "path": "https://res.cloudinary.com/dqo9rn5lp/image/upload/v1691299713/geoguesserimages-pclub/5.jpg",
-        "long": 22.5202903,
-        "lat": 75.9202886
-      },
-      {
-        "_id": 6,
-        "path": "https://res.cloudinary.com/dqo9rn5lp/image/upload/v1691299710/geoguesserimages-pclub/6.jpg",
-        "long": 22.520688,
-        "lat": 75.9203637
-      },
-      {
-        "_id": 7,
-        "path": "https://res.cloudinary.com/dqo9rn5lp/image/upload/v1691299710/geoguesserimages-pclub/7.jpg",
-        "long": 22.5216567,
-        "lat": 75.9216575
-      },
-      {
-        "_id": 8,
-        "path": "https://res.cloudinary.com/dqo9rn5lp/image/upload/v1691299711/geoguesserimages-pclub/8.jpg",
-        "long": 22.5216567,
-        "lat": 75.9216575
-      },
-      {
-        "_id": 9,
-        "path": "https://res.cloudinary.com/dqo9rn5lp/image/upload/v1691299714/geoguesserimages-pclub/9.jpg",
-        "long": 22.523077,
-        "lat": 75.9254911
-      },
-      {
-        "_id": 10,
-        "path": "https://res.cloudinary.com/dqo9rn5lp/image/upload/v1691299712/geoguesserimages-pclub/10.jpg",
-        "long": 22.5231328,
-        "lat": 75.9228052
-      },
-      {
-        "_id": 11,
-        "path": "https://res.cloudinary.com/dqo9rn5lp/image/upload/v1691299708/geoguesserimages-pclub/11.jpg",
-        "long": 22.5241666,
-        "lat": 75.9255987
-      },
-      {
-        "_id": 12,
-        "path": "https://res.cloudinary.com/dqo9rn5lp/image/upload/v1691299712/geoguesserimages-pclub/12.jpg",
-        "long": 22.524637,
-        "lat": 75.9220196
-      },
-      {
-        "_id": 13,
-        "path": "https://res.cloudinary.com/dqo9rn5lp/image/upload/v1691299708/geoguesserimages-pclub/13.jpg",
-        "long": 22.5253896,
-        "lat": 75.9207801
-      },
-      {
-        "_id": 14,
-        "path": "https://res.cloudinary.com/dqo9rn5lp/image/upload/v1691299711/geoguesserimages-pclub/14.jpg",
-        "long": 22.5259105,
-        "lat": 75.9263269
-      },
-      {
-        "_id": 15,
-        "path": "https://res.cloudinary.com/dqo9rn5lp/image/upload/v1691299708/geoguesserimages-pclub/15.jpg",
-        "long": 22.526821,
-        "lat": 75.9232974
-      },
-      {
-        "_id": 16,
-        "path": "https://res.cloudinary.com/dqo9rn5lp/image/upload/v1691299712/geoguesserimages-pclub/16.jpg",
-        "long": 22.526821,
-        "lat": 75.9232974
-      },
-      {
-        "_id": 17,
-        "path": "https://res.cloudinary.com/dqo9rn5lp/image/upload/v1691299710/geoguesserimages-pclub/17.jpg",
-        "long": 22.526821,
-        "lat": 75.9232974
-      },
-      {
-        "_id": 18,
-        "path": "https://res.cloudinary.com/dqo9rn5lp/image/upload/v1691299709/geoguesserimages-pclub/18.jpg",
-        "long": 22.526821,
-        "lat": 75.9232974
-      },
-      {
-        "_id": 19,
-        "path": "https://res.cloudinary.com/dqo9rn5lp/image/upload/v1691299712/geoguesserimages-pclub/19.jpg",
-        "long": 22.526821,
-        "lat": 75.9232974
-      },
-      {
-        "_id": 20,
-        "path": "https://res.cloudinary.com/dqo9rn5lp/image/upload/v1691299708/geoguesserimages-pclub/20.jpg",
-        "long": 22.526821,
-        "lat": 75.9232974
-      },
-      {
-        "_id": 21,
-        "path": "https://res.cloudinary.com/dqo9rn5lp/image/upload/v1691299712/geoguesserimages-pclub/21.jpg",
-        "long": 22.526821,
-        "lat": 75.9232974
-      },
-      {
-        "_id": 22,
-        "path": "https://res.cloudinary.com/dqo9rn5lp/image/upload/v1691299711/geoguesserimages-pclub/22.jpg",
-        "long": 22.526821,
-        "lat": 75.9232974
-      },
-      {
-        "_id": 23,
-        "path": "https://res.cloudinary.com/dqo9rn5lp/image/upload/v1691299710/geoguesserimages-pclub/23.jpg",
-        "long":22.5241666,
-        "lat":75.9255987
-        
-      },
-      {
-        "_id": 24,
-        "path": "https://res.cloudinary.com/dqo9rn5lp/image/upload/v1691299713/geoguesserimages-pclub/24.jpg",
-        "long":22.5302561,
-        "lat":75.9229349
-      },
-      {
-        "_id": 25,
-        "path": "https://res.cloudinary.com/dqo9rn5lp/image/upload/v1691299709/geoguesserimages-pclub/25.jpg",
-        "long": 22.5200828,
-        "lat": 75.9233487
-      },
-      {
-        "_id": 26,
-        "path": "https://res.cloudinary.com/dqo9rn5lp/image/upload/v1691299712/geoguesserimages-pclub/26.jpg",
-        "long": 22.5291638,
-        "lat": 75.9222952
-      },
-      {
-        "_id": 27,
-        "path": "https://res.cloudinary.com/dqo9rn5lp/image/upload/v1691299709/geoguesserimages-pclub/27.jpg",
-        "long": 22.5216567,
-        "lat": 75.9216575
-      }
+      "_id": 2,
+      "path": "https://res.cloudinary.com/dqo9rn5lp/image/upload/v1691299710/geoguesserimages-pclub/2.jpg",
+      "long": 22.5235085,
+      "lat": 75.9254713
+    },
+    {
+      "_id": 3,
+      "path": "https://res.cloudinary.com/dqo9rn5lp/image/upload/v1691299713/geoguesserimages-pclub/3.jpg",
+      "long":22.5303081,
+      "lat":75.9230680
+    },
+    {
+      "_id": 4,
+      "path": "https://res.cloudinary.com/dqo9rn5lp/image/upload/v1691299711/geoguesserimages-pclub/4.jpg",
+      "long": 22.5268210,
+      "lat": 75.9232974
+    },
+    {
+      "_id": 5,
+      "path": "https://res.cloudinary.com/dqo9rn5lp/image/upload/v1691299713/geoguesserimages-pclub/5.jpg",
+      "long": 22.5296540,
+      "lat": 75.9251233
+    },
+    {
+      "_id": 6,
+      "path": "https://res.cloudinary.com/dqo9rn5lp/image/upload/v1691299710/geoguesserimages-pclub/6.jpg",
+      "long": 22.5246370,
+      "lat": 75.9220196
+    },
+    {
+      "_id": 7,
+      "path": "https://res.cloudinary.com/dqo9rn5lp/image/upload/v1691299710/geoguesserimages-pclub/7.jpg",
+      "long": 22.5231328,
+      "lat": 75.9228052
+    },
+    {
+      "_id": 8,
+      "path": "https://res.cloudinary.com/dqo9rn5lp/image/upload/v1691299711/geoguesserimages-pclub/8.jpg",
+      "long": 22.5278061,
+      "lat": 75.9252302
+    },
+    {
+      "_id": 9,
+      "path": "https://res.cloudinary.com/dqo9rn5lp/image/upload/v1691299714/geoguesserimages-pclub/9.jpg",
+      "long": 22.5321919,
+      "lat": 75.9202571
+    },
+    {
+      "_id": 10,
+      "path": "https://res.cloudinary.com/dqo9rn5lp/image/upload/v1691299712/geoguesserimages-pclub/10.jpg",
+      "long": 22.5259105,
+      "lat": 75.9263269
+    },
+    {
+      "_id": 11,
+      "path": "https://res.cloudinary.com/dqo9rn5lp/image/upload/v1691299708/geoguesserimages-pclub/11.jpg",
+      "long": 22.5191713,
+      "lat": 75.9198246
+    },
+    {
+      "_id": 12,
+      "path": "https://res.cloudinary.com/dqo9rn5lp/image/upload/v1691299712/geoguesserimages-pclub/12.jpg",
+      "long": 22.5289712,
+      "lat": 75.9221581
+    },
+    {
+      "_id": 13,
+      "path": "https://res.cloudinary.com/dqo9rn5lp/image/upload/v1691299708/geoguesserimages-pclub/13.jpg",
+      "long": 22.5202184,
+      "lat": 75.9204375
+    },
+    {
+      "_id": 14,
+      "path": "https://res.cloudinary.com/dqo9rn5lp/image/upload/v1691299711/geoguesserimages-pclub/14.jpg",
+      "long": 22.5283710,
+      "lat": 75.9216240
+    },
+    {
+      "_id": 15,
+      "path": "https://res.cloudinary.com/dqo9rn5lp/image/upload/v1691299708/geoguesserimages-pclub/15.jpg",
+      "long": 22.5199332,
+      "lat": 75.9200787
+    },
+    {
+      "_id": 16,
+      "path": "https://res.cloudinary.com/dqo9rn5lp/image/upload/v1691299712/geoguesserimages-pclub/16.jpg",
+      "long": 22.5294094,
+      "lat": 75.9251974
+    },
+    {
+      "_id": 17,
+      "path": "https://res.cloudinary.com/dqo9rn5lp/image/upload/v1691299710/geoguesserimages-pclub/17.jpg",
+      "long": 22.5230770,
+      "lat": 75.9254911
+    },
+    {
+      "_id": 18,
+      "path": "https://res.cloudinary.com/dqo9rn5lp/image/upload/v1691299709/geoguesserimages-pclub/18.jpg",
+      "long": 22.5202903,
+      "lat": 75.9202886
+    },
+    {
+      "_id": 19,
+      "path": "https://res.cloudinary.com/dqo9rn5lp/image/upload/v1691299712/geoguesserimages-pclub/19.jpg",
+      "long": 22.5293255,
+      "lat": 75.9264138
+    },
+    {
+      "_id": 20,
+      "path": "https://res.cloudinary.com/dqo9rn5lp/image/upload/v1691299708/geoguesserimages-pclub/20.jpg",
+      "long": 22.5206880,
+      "lat": 75.9203637
+    },
+    {
+      "_id": 21,
+      "path": "https://res.cloudinary.com/dqo9rn5lp/image/upload/v1691299712/geoguesserimages-pclub/21.jpg",
+      "long": 22.5293608,
+      "lat": 75.9255632
+    },
+    {
+      "_id": 22,
+      "path": "https://res.cloudinary.com/dqo9rn5lp/image/upload/v1691299711/geoguesserimages-pclub/22.jpg",
+      "long": 22.5253896,
+      "lat": 75.9207801
+    },
+    {
+      "_id": 23,
+      "path": "https://res.cloudinary.com/dqo9rn5lp/image/upload/v1691299710/geoguesserimages-pclub/23.jpg",
+      "long": 22.5241666,
+      "lat": 75.9255987
+    },
+    {
+      "_id": 24,
+      "path": "https://res.cloudinary.com/dqo9rn5lp/image/upload/v1691299713/geoguesserimages-pclub/24.jpg",
+      "long": 22.5302561,
+      "lat": 75.9229349
+    },
+    {
+      "_id": 25,
+      "path": "https://res.cloudinary.com/dqo9rn5lp/image/upload/v1691299709/geoguesserimages-pclub/25.jpg",
+      "long": 22.5200828,
+      "lat": 75.9233487
+    },
+    {
+      "_id": 26,
+      "path": "https://res.cloudinary.com/dqo9rn5lp/image/upload/v1691299712/geoguesserimages-pclub/26.jpg",
+      "long": 22.5291638,
+      "lat": 75.9222952
+    },
+    {
+      "_id": 27,
+      "path": "https://res.cloudinary.com/dqo9rn5lp/image/upload/v1691299709/geoguesserimages-pclub/27.jpg",
+      "long": 22.5216567,
+      "lat": 75.9216575
+    }
+
 ]
 
 module.exports = images;
